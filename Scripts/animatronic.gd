7b3eabb8904b662cb9ffaebecbbf424dae1e1bee
Chamerli <fares.setbel@proton.me>
2026-02-10 19:53:14 +0100

    fixed the followed path system and tried the subclasses

--- Scripts/animatronic.gd
+++ Scripts/animatronic.gd
@@ -9,8 +9,8 @@ var current_room: int
 @export var ai_difficulty: int
 @export var mov_opp_delay: int = 3
 
-@export var path_phase_index: Dictionary[int, int] = {} # Cam, AmiuntOfPhases
 @export var path_order: Array[int]
+@export var phase_order: Array[int]
 
 @export var is_free_roam: bool
 
@@ -19,6 +19,7 @@ var current_room: int
 
 
 func _ready() -> void:
+	update_sprite(Global.currect_cam)
 	movement_opportunity_timer.wait_time = mov_opp_delay
 	hide()
 	current_room = starting_room
@@ -26,9 +27,7 @@ func _ready() -> void:
 	movement_opportunity_timer.timeout.connect(_on_try_movement)
 
 func _on_camera_changed(new_cam: int):
-	if current_room == new_cam:
-		show()
-	else: hide()
+	update_sprite(new_cam)
 
 func _on_try_movement():
 	if is_free_roam:
@@ -43,24 +42,35 @@ var phase: int = 0
 var path_pos: int = 0
 
 func move_forward_path() -> void:
-	var max_phase = path_phase_index.get(current_room, 1)
+	if path_pos > path_order.size() - 1: return
+	var max_phase = phase_order[path_pos]
 	
-	print("Phase:", phase, "Current room:", current_room)
 	
-	if phase < max_phase - 1:
+	#print(max_phase)
+	
+	print("Phase: %s Current room: %s" % [phase, current_room])
+	
+	if phase < max_phase - 1: # advance phase
 		phase += 1
-	else:
+		#print(phase)
+	else: # move to the next room
 		phase = 0
 		path_pos += 1
-		if path_pos >= path_order.size():
-			is_in_office()
+		if path_pos > path_order.size() - 1:
+			finish_path()
 			return
+		
 		current_room = path_order[path_pos]
 
-	update_sprite()
+	update_sprite(Global.currect_cam)
+
+func finish_path():
+	pass
 
 func free_roam_move() -> void:
-	update_sprite()
+	update_sprite(Global.currect_cam)
 
-func update_sprite() -> void:
-	pass
+func update_sprite(camera_index: int) -> void:
+	if current_room == camera_index:
+		show()
+	else: hide()
---
+++ Scripts/animatronic_sub.gd
@@ -0,0 +1,5 @@
+extends Animatronic
+
+
+func finish_path():
+	print("je")
